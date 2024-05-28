7eba7e81ab98a44fad6626e14094d0d7d7a9423c
Muhammed Mehmet Tanit <Muhammed.Tanit@Student.HTW-Berlin.de>
2024-05-28 20:44:44 +0200

    Controller überarbeitet

--- src/main/java/de/htwberlin/webtech/Healthtracker/Controller/UserController.java
+++ src/main/java/de/htwberlin/webtech/Healthtracker/Controller/UserController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:5174", "http://127.0.0.1:5174", "https://Healthtracker-frontend.onrender.com/"})
+@CrossOrigin(origins = {"http://localhost:5174", "http://127.0.0.1:5174", "https://healthtracker-frontend.onrender.com/"})
 public class UserController {
 
     @GetMapping("/Users")
