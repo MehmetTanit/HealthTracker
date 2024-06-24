fe2197ff13c7f379d7821b25fc26799c074194b7
Muhammed Mehmet Tanit <Muhammed.Tanit@Student.HTW-Berlin.de>
2024-06-24 14:44:48 +0200

    controller hinzugefügt

--- src/main/java/de/htwberlin/webtech/Healthtracker/Controller/SleepPatternController.java
+++ src/main/java/de/htwberlin/webtech/Healthtracker/Controller/SleepPatternController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173", "https://healthtracker-frontend.onrender.com" })
 @RequestMapping("/SleepPattern")
 public class SleepPatternController {
 
